8152e1734bee6a04e5ba9d31b0aef168eaa29061
kondzio <semmlerko>
2019-11-30 16:09:45 +0100

    Updated sql dump scripts
    Should work with XAMPP for Ubuntu now

--- dump_db.sh
+++ dump_db.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if (( $(uname) == "Darwin")) ; then 
+	mysql_loc=/Applications/xampp/xamppfiles/bin/mysqldump
+else 
+	mysql_loc=/opt/lampp/bin/mysqldump
+fi
+
 # For mac (this is Xampps mysql location)
-/Applications/xampp/xamppfiles/bin/mysqldump --user='root' --password='' wordpress_base > wp_dump_new.sql
+"$mysql_loc" --user='root' --password='' wordpress_base > wp_dump_new.sql
--- update_db.sh
+++ update_db.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-# For Mac (uses Xampp mysql location)
-/Applications/xampp/xamppfiles/bin/mysql --user='root' --password='' wordpress_base < wp_dump.sql
+if (( $(uname) == "Darwin")) ; then 
+	mysql_loc=/Applications/xampp/xamppfiles/bin/mysql
+else 
+	mysql_loc=/opt/lampp/bin/mysql
+fi
+
+"$mysql_loc" --user='root' --password='' wordpress_base < wp_dump.sql
