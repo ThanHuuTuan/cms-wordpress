6bd94946d0a8d96b7693be50394560e3de2ac119
kondzio <semmlerko>
2020-01-19 18:26:23 +0100

    Updated wordpress installation

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'wordpress_base' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'pV2PGr3AV3DN7BfD' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
