3d4277de082816fbfb58d8bb9c1d5910036d5a42
semmlerkonrad <42716611+semmlerkonrad@users.noreply.github.com>
2020-03-31 22:18:38 +0200

    Update comment

--- dump_db.sh
+++ dump_db.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if (( $(uname) == "Darwin")) ; then 
+	# For mac (this is Xampps mysql location)
 	mysql_loc=/Applications/xampp/xamppfiles/bin/mysqldump
 else 
 	mysql_loc=/opt/lampp/bin/mysqldump
 fi
 
-# For mac (this is Xampps mysql location)
 "$mysql_loc" --user='root' --password='' wordpress_base > wp_dump_new.sql
